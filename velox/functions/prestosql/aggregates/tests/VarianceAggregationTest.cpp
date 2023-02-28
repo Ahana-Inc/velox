4fd41e87c8064faab79e2d7bdaf9fc8a8d565a1a
Ivan Sadikov <ivan@ahana.io>
2023-03-01 08:06:46 +1300

    u

--- velox/functions/prestosql/aggregates/tests/VarianceAggregationTest.cpp
+++ velox/functions/prestosql/aggregates/tests/VarianceAggregationTest.cpp
@@ -174,6 +174,7 @@ TEST_F(VarianceAggregationTest, varianceWithGlobalAggregation) {
         vectors,
         {},
         {GEN_AGG("c1"), GEN_AGG("c2"), GEN_AGG("c4"), GEN_AGG("c5")},
+        {"a0", "a1", "a2", "a3"},
         sql);
 
     // Global aggregation; no input
